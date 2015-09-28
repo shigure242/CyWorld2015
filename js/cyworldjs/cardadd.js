d6b5135aba914a3a4b06ce82fbe1bd3d6a97f5a9
unknown <x13g033@PC402N007.chiba-fjb.ac.jp>
2015-09-28 09:54:19 +0900

    jsにこめんといれた。 20150928

--- js/cyworldjs/cardadd.js
+++ js/cyworldjs/cardadd.js
@@ -1,13 +1,23 @@
+/*
+  z-index
+  cardadd:103
+  card-tmp:102
+  bg-overlay:100
+*/
 $("docment").ready(function() {
+  //add
   $("#cardadd").on("click", function() {
-    if ($("#bg-overlay").css('opacity') == 0 || $("#bg-overlay").css('opacity') == 1) {
+    if ($("#bg-overlay").css('opacity') == 0 || $("#bg-overlay").css('opacity') == 1) {//Barrage chk
+      //fadein
       if ($("#bg-overlay").css("display") == "none") {
         $("#bg-overlay").fadeIn();
         $("#card-tmp").fadeIn();
+      //fadeout
       } else {
         var c = $("#card-tmp").clone(true);
         $("#bg-overlay").fadeOut();
         $("#card-tmp").fadeOut();
+        //add text
         var input=c.find('input');
         input.after(c.find('input').val());
         input.remove();
@@ -16,11 +26,12 @@ $("docment").ready(function() {
         textarea.remove();
         c.addClass("col");
         c.addClass("s3");
-        c.removeAttr("id");
+        c.removeAttr("id");//Duplicate measures
         $("#main-content").append(c);
       }
     }
   });
+  //notadd overlay off
   $("#bg-overlay").on("click",function(){
     $("#bg-overlay").fadeOut();
     $("#card-tmp").fadeOut();
